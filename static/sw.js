b0310fe9a80380497a8eb21796799c7a5ab96768
yangoogle <yang657850144@gmail.com>
2019-03-28 10:54:46 +0800

    feat: 新增缓存文件白名单

--- static/sw.js
+++ static/sw.js
@@ -1,5 +1,9 @@
 let mainCacheFiles = [
-  'index.html'
+  'index.html',
+  'js',
+  'css',
+  'png',
+  'jpg'
 ]
 
 let version = 'cache-version1'
@@ -39,30 +43,42 @@ self.addEventListener('activate', function (evt) {
     })
   )
 })
+// 白名单过滤
+function filterAssetRequest (url) {
+  let result = mainCacheFiles.some(elem => {
+    let reg = new RegExp(`${elem}$`)
+    return url.match(reg) > -1
+  })
+  return result
+}
 
 // 请求拦截
 self.addEventListener('fetch', function (evt) {
-  // console.log('处理fetch事件:', evt.request.url)
-  // if (evt.request.url.match('sockjs')) return
-  // evt.respondWith(
-  //   caches.match(evt.request).then(function (response) {
-  //     if (response) {
-  //       console.log('缓存匹配到res:', response)
-  //       // return response
-  //     }
-  //     // console.log('缓存未匹配对应request,准备从network获取', caches)
-  //     return fetch(evt.request).then(function (response) {
-  //       console.log('req', evt.request, response.clone())
-  //       let cpResponse = response.clone()
-  //       // console.log('fetch获取到的response:', response)
-  //       caches.open(version).then(function (cache) {
-  //         cache.put(evt.request, cpResponse)
-  //       })
-  //       return response
-  //     })
-  //   }).catch(function (err) {
-  //     console.error('fetch 接口错误', err)
-  //     throw err
-  //   })
-  // )
+  const url = evt.request.url
+  let assetMatch = filterAssetRequest(url)
+  if (url.match('sockjs')) return
+  let isGetMethod = evt.request.method === 'GET'
+  // 过滤 非GET请求 | 非白名单url
+  if (!isGetMethod || !assetMatch) return
+
+  evt.respondWith(
+    caches.match(evt.request).then(function (response) {
+      if (response) {
+        return response
+      }
+      // console.log('缓存未匹配对应request,准备从network获取', caches)
+      return fetch(evt.request).then(function (response) {
+        // console.log('req', evt.request, response.clone())
+        let cpResponse = response.clone()
+        // console.log('fetch获取到的response:', response)
+        caches.open(version).then(function (cache) {
+          cache.put(evt.request, cpResponse)
+        })
+        return response
+      })
+    }).catch(function (err) {
+      console.error('fetch 接口错误', err)
+      throw err
+    })
+  )
 })
