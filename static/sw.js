8c8a03ca536a4a8376cab8b44662d1a2aa9d3ceb
yangoogle <yang657850144@gmail.com>
2019-03-28 09:39:25 +0800

    fix: remove fetch cache

--- static/sw.js
+++ static/sw.js
@@ -43,26 +43,26 @@ self.addEventListener('activate', function (evt) {
 // 请求拦截
 self.addEventListener('fetch', function (evt) {
   // console.log('处理fetch事件:', evt.request.url)
-  if (evt.request.url.match('sockjs')) return
-  evt.respondWith(
-    caches.match(evt.request).then(function (response) {
-      if (response) {
-        console.log('缓存匹配到res:', response)
-        // return response
-      }
-      // console.log('缓存未匹配对应request,准备从network获取', caches)
-      return fetch(evt.request).then(function (response) {
-        console.log('req', evt.request, response.clone())
-        let cpResponse = response.clone()
-        // console.log('fetch获取到的response:', response)
-        caches.open(version).then(function (cache) {
-          cache.put(evt.request, cpResponse)
-        })
-        return response
-      })
-    }).catch(function (err) {
-      console.error('fetch 接口错误', err)
-      throw err
-    })
-  )
+  // if (evt.request.url.match('sockjs')) return
+  // evt.respondWith(
+  //   caches.match(evt.request).then(function (response) {
+  //     if (response) {
+  //       console.log('缓存匹配到res:', response)
+  //       // return response
+  //     }
+  //     // console.log('缓存未匹配对应request,准备从network获取', caches)
+  //     return fetch(evt.request).then(function (response) {
+  //       console.log('req', evt.request, response.clone())
+  //       let cpResponse = response.clone()
+  //       // console.log('fetch获取到的response:', response)
+  //       caches.open(version).then(function (cache) {
+  //         cache.put(evt.request, cpResponse)
+  //       })
+  //       return response
+  //     })
+  //   }).catch(function (err) {
+  //     console.error('fetch 接口错误', err)
+  //     throw err
+  //   })
+  // )
 })
